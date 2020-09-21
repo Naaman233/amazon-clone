e96776ea421bde6764c62157e6a70ed8c710c96e
Makafui Naaman <makafuinaaman@Makafuis-MacBook-Pro.local>
2020-09-21 14:15:10 -0400

    setting up initial project architecture.

---
+++ amazon-clone/firebase/index.ts
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB4NvyARnaJFxSWAzuNsexWxXbeGrp5yfM",
+    authDomain: "clone-f7858.firebaseapp.com",
+    databaseURL: "https://clone-f7858.firebaseio.com",
+    projectId: "clone-f7858",
+    storageBucket: "clone-f7858.appspot.com",
+    messagingSenderId: "739916545209",
+    appId: "1:739916545209:web:8fb2aa3925d9dc9afb2f68",
+    measurementId: "G-MV0ML1QTTK"
+  };
+  //export default firebase.initializeApp(firebaseConfig);
+
+  
