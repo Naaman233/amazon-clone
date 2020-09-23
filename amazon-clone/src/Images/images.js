feeecf283c1cfe8dda403926c02b50cb6e1b5503
Makafui Naaman <makafuinaaman@Makafuis-MacBook-Pro.local>
2020-09-23 03:55:04 -0400

    Implemented web banner to the home page

---
+++ amazon-clone/src/Images/images.js
@@ -0,0 +1,8 @@
+/** Slideshow Jack Ryan  */
+//<img alt="Amazon Original Tom Clancy's Jack Ryan. Watch the new season on Prime Video" src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2019/TV/JKRY_S2/JKRY_S2_01473_GWBleedingHero_3000x1200_POST_Final_en_US_PVD3016._CB448982223_.jpg" height="600px" width="1500px" data-a-hires="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2019/TV/JKRY_S2/JKRY_S2_01473_GWBleedingHero_3000x1200_POST_Final_en_US_PVD3016._CB448982223_.jpg"></img>
+
+/**Slideshow Halo Amazon */
+//<img alt="Introducing Halo. Activity + Sleep + Tone + Body Composition" src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/YjJkZWQ0NTgt/YjJkZWQ0NTgt-NGU5NjdkODIt-w3000._CB406119060_.jpg" height="600px" width="1500px" data-a-hires="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/YjJkZWQ0NTgt/YjJkZWQ0NTgt-NGU5NjdkODIt-w3000._CB406119060_.jpg"></img>
+
+/** Slideshow Amazon Podcast */
+//<img alt="Podcasts now streaming on Amazon Music" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonMusic/2020/RedHood/Launch/GW/US-EN_080320_RedHood_ACQ_GW_Hero_D_3000x1200_CV2._CB406776641_.jpg" height="600px" width="1500px" data-a-hires="https://images-na.ssl-images-amazon.com/images/G/01/AmazonMusic/2020/RedHood/Launch/GW/US-EN_080320_RedHood_ACQ_GW_Hero_D_3000x1200_CV2._CB406776641_.jpg"></img>
